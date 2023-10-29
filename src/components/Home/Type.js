9ebf1c100a1c1076af9c7c06a93c2289303bac9b
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-10-30 03:40:44 +0530

    same as prev

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -3,32 +3,18 @@ import Typewriter from "typewriter-effect";
 
 function Type() {
   return (
-    <div>
-      <Typewriter
-        options={{
-          strings: ["Astrophysics"],
-          autoStart: true,
-          loop: false,
-          deleteSpeed: 0,
-        }}
-      />
-      <Typewriter
-        options={{
-          strings: ["Planetary Sciences"],
-          autoStart: true,
-          loop: false,
-          deleteSpeed: 0,
-        }}
-      />
-      <Typewriter
-        options={{
-          strings: ["Machine Learning & AI"],
-          autoStart: true,
-          loop: false,
-          deleteSpeed: 0,
-        }}
-      />
-    </div>
+    <Typewriter
+      options={{
+        strings: [
+          "Astrophysics",
+          "Planetary Sciences",
+          "Machine Learning & AI",
+        ],
+        autoStart: false,
+        loop: false,
+        deleteSpeed: 50
+      }}
+    />
   );
 }
 
