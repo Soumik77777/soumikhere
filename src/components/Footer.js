4d189748b26e67cc17114ae0f46d9c7083f15142
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-10-31 03:23:42 +0530

    footer

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,7 +5,7 @@ import {
   AiOutlineTwitter,
   AiFillInstagram,
 } from "react-icons/ai";
-import { FaLinkedinIn } from "react-icons/fa";
+import { FaResearchgate, FaLinkedinIn } from "react-icons/fa";
 
 function Footer() {
   let date = new Date();
@@ -14,7 +14,16 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>Fork the site template</h3>
+          <h3>
+            Fork the{" "}
+            <a
+              href="https://github.com/soumyajit4419/Portfolio"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              site template
+            </a>
+          </h3>
         </Col>
         <Col md="4" className="footer-copywright">
           <h3>Copyright © {year} SB</h3>
@@ -33,17 +42,17 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://twitter.com/Soumyajit4419"
+                href="https://www.researchgate.net/profile/Soumik-Bhattacharyya"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
               >
-                <AiOutlineTwitter />
+                <FaResearchgate />
               </a>
             </li>
             <li className="social-icons">
               <a
-                href="https://www.linkedin.com/in/soumyajit4419/"
+                href="https://www.linkedin.com/in/soumik-bhattacharyya-48446a170/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
@@ -53,7 +62,7 @@ function Footer() {
             </li>
             <li className="social-icons">
               <a
-                href="https://www.instagram.com/soumyajit4419"
+                href="https://www.instagram.com/soumik.py/"
                 style={{ color: "white" }}
                 target="_blank" 
                 rel="noopener noreferrer"
