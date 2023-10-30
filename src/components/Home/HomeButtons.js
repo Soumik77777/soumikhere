357a7da00db952a6f82eea1875ba8b2493c72afd
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-10-31 02:40:41 +0530

    buttons

--- src/components/Home/HomeButtons.js
+++ src/components/Home/HomeButtons.js
@@ -3,45 +3,59 @@ import Button from "react-bootstrap/Button";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
 import "./HomeButtons.css";
-import { BsFillArrowRightCircleFill} from "react-icons/bs"
+import { BsFillArrowRightCircleFill } from "react-icons/bs";
 
 function HomeButtons() {
-  const buttonStyle = {
-    backgroundColor: "#ffff00",
-    color: "#000000",
-    width: "325px",
-    margin: "80px 200px",
-  };
-
   return (
     <Row>
       <Col sm={4}>
+        const aboutButtonStyle = {
+          backgroundColor: "#ffff00",
+          color: "#000000",
+          width: "325px",
+          margin: "80px 0px 80px 200px", // Adjust margin as needed
+        };
+
         <Button
           className="about-button-link custom-button"
           variant="outline-info"
           size="lg"
           href={"/about"}
           target="_self"
-          style={buttonStyle}
+          style={aboutButtonStyle}
         >
           Learn about me <BsFillArrowRightCircleFill />
         </Button>
       </Col>
 
       <Col sm={4}>
+        const projectsButtonStyle = {
+          backgroundColor: "#ffff00",
+          color: "#000000",
+          width: "325px",
+          margin: "80px 0px 80px 0px", // Adjust margin as needed
+        };
+
         <Button
           className="projects-button-link custom-button"
           variant="outline-info"
           size="lg"
           href={"/projects"}
           target="_self"
-          style={buttonStyle}
+          style={projectsButtonStyle}
         >
           Read about my research <BsFillArrowRightCircleFill />
         </Button>
       </Col>
 
       <Col sm={4}>
+        const blogButtonStyle = {
+          backgroundColor: "#ffff00",
+          color: "#000000",
+          width: "325px",
+          margin: "80px 200px 80px 0px", // Adjust margin as needed
+        };
+
         <Button
           className="projects-button-link custom-button"
           variant="outline-info"
@@ -49,7 +63,7 @@ function HomeButtons() {
           href={"#"}
           target="_self"
           disabled
-          style={buttonStyle}
+          style={blogButtonStyle}
         >
           Blog posts (coming...)
         </Button>
