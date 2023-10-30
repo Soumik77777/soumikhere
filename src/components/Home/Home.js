547bef8af8232abdd0d9e934c24ebfcc43701463
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-10-30 16:28:54 +0530

    same

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import homeLogo from "../../Assets/home-main_2.svg";
+import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
