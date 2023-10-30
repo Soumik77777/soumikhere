75f526270cad2675b1acd922757ceb53b5ad9eb5
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-10-30 23:51:20 +0530

    type

--- src/components/Home/Type.js
+++ src/components/Home/Type.js
@@ -1,22 +1,26 @@
-import React from "react";
-import Typewriter from "typewriter-effect";
+import React, { useState, useEffect } from "react";
+import "./Type.css";
 
 function Type() {
-  return (
-    <Typewriter
-      options={{
-        strings: [
-          "Astrophysics",
-          "Planetary Sciences",
-          "Machine Learning & AI",
-        ],
-        autoStart: true,
-        loop: false,
-        pauseFor: 1000,
-        deleteSpeed: 50
-      }}
-    />
-  );
+  const [text, setText] = useState("");
+  const fullText = "Astrophysics, Planetary Sciences, Machine Learning & AI";
+
+  useEffect(() => {
+    let currentIndex = 0;
+
+    const interval = setInterval(() => {
+      if (currentIndex < fullText.length) {
+        setText(fullText.slice(0, currentIndex + 1));
+        currentIndex += 1;
+      } else {
+        clearInterval(interval);
+      }
+    }, 100); // Adjust the interval for typing speed
+
+    return () => clearInterval(interval);
+  }, []);
+
+  return <div className="typing-effect">{text}</div>;
 }
 
 export default Type;
