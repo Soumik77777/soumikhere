f67d1e5b3e8e33508ef7c9275111904c950a3acc
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-04 16:35:52 +0530

    cardblog

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -21,10 +21,10 @@ function Projects() {
           Here are a few projects I've worked on recently.
         </p>
         <Row style={{ justifyContent: "center", paddingBottom: "10px" }}>
-          <Col md={4} className="project-card">
+          <Col md={4} className="blog-card">
             <ProjectCard
               imgPath={chatify}
-              isBlog={false}
+              isBlog={true}
               title="Chatify"
               description="Personal Chat Room or Workspace to share resources and hangout with friends build with react.js, Material-UI, and Firebase. Have features which allows user for realtime messaging, image sharing as well as supports reactions on messages."
               ghLink="https://github.com/soumyajit4419/Chatify"
