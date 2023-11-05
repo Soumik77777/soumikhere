6590625ddd8371c4343d217492b2ed8eb4b601f7
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-05 18:02:34 +0530

    padding

--- src/components/Home/Home2.js
+++ src/components/Home/Home2.js
@@ -16,7 +16,7 @@ function Home2() {
     <Container fluid className="home-about-section" id="about">
       <Container>
         <Row>
-          <Col md={12} className="home-about-description">
+          <Col md={12} className="home-about-header">
             <h1 style={{ fontSize: "2.6em" }}>
               LET ME <span className="purple"> INTRODUCE </span> MYSELF
             </h1>
