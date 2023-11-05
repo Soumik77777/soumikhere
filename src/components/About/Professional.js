8b1cffad9c2762a480103c6cc074f868e77f2287
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-05 21:13:14 +0530

    try

--- src/components/About/Professional.js
+++ src/components/About/Professional.js
@@ -7,25 +7,21 @@ function App() {
       <div className="row">
         <div className="col-md-8">
           <p>
-            This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph. It contains some content that will appear to the left of the card.
+            This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph.This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. 
           </p>
-        </div>
-        <div className="col-md-4">
-          <Card>
-            <Card.Body>
-              <Card.Title>Card Title</Card.Title>
-              <Card.Text>
-                This is the content of the card. You can place any relevant
-                information, images, or components here.
-              </Card.Text>
-            </Card.Body>
-          </Card>
-        </div>
-      </div>
-      <div className="row">
-        <div className="col-md-8">
+          <div className="card-wrapper">
+            <Card>
+              <Card.Body>
+                <Card.Title>Card Title</Card.Title>
+                <Card.Text>
+                  This is the content of the card. You can place any relevant
+                  information, images, or components here.
+                </Card.Text>
+              </Card.Body>
+            </Card>
+          </div>
           <p>
-            This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. This text continues below the card, and you can add more content as needed. 
+            This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. 
           </p>
         </div>
       </div>
