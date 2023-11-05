c59b64c05fbbd5ff98e1be2b0a14cbb4e5ea4ba4
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-05 22:08:47 +0530

    about-prof

---
+++ src/components/About/AboutCards.js
@@ -0,0 +1,109 @@
+import React, { useState } from "react";
+import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
+import { CgWebsite } from "react-icons/cg";
+import { BsGithub } from "react-icons/bs";
+
+function AboutCards(props) {
+  const [expanded, setExpanded] = useState(false);
+
+  const toggleDescription = () => {
+    setExpanded(!expanded);
+  };
+
+  return (
+    <Card className="project-card-view">
+      {props.header && <Card.Header style={{
+        fontSize: "0.9em",
+        textAlign: "left",
+        //marginBottom: "20px",
+        borderBottom: "3px solid #371D41"
+        }}>{props.header}</Card.Header>}
+      <Card.Img variant="top" src={props.imgPath} alt="card-img" />
+      <Card.Body>
+        <Card.Title style={{ textAlign: "left", marginBottom: "10px", color: "#ffffc1"}}>{props.title}</Card.Title>
+        {props.subtitle && (
+          <Card.Subtitle className="mb-2 text-muted" style={{textAlign: "left", marginBottom: "60px" }}>{props.subtitle}</Card.Subtitle>
+        )}
+        <Card.Text style={{ textAlign: "justify"}}>
+          {expanded ? props.description : `${props.description.slice(0, 300)}...`}
+          {props.description.length > 300 && !expanded && (
+            <span>
+              {" "}
+              <Button
+                variant="link"
+                size="sm"
+                onClick={toggleDescription}
+              >
+                Read more
+              </Button>
+            </span>
+          )}
+          {props.description.length > 300 && expanded && (
+            <span>
+              {" "}
+              <Button
+                variant="link"
+                size="sm"
+                onClick={toggleDescription}
+              >
+                Read less
+              </Button>
+            </span>
+          )}
+        </Card.Text>
+
+        {props.islink && (
+          <Button 
+            variant="primary"
+            href={props.link}
+            target="_blank"
+            style={{ marginLeft: "10px", marginRight: "10px" }}
+          >
+            {props.linktext}
+          </Button>
+        )}
+
+        {props.islink2 && (
+          <Button 
+            variant="primary"
+            href={props.link2}
+            target="_blank"
+            style={{ marginLeft: "10px", marginRight: "10px" }}
+          >
+            {props.linktext2}
+          </Button>
+        )}
+
+        
+        
+        {/*}
+        <Button variant="primary"
+        href={props.ghLink}
+        target="_blank"
+        >
+          <BsGithub /> &nbsp;
+          {props.isBlog ? "Blog" : "GitHub"}
+        </Button>
+        {"\n"}
+        {"\n"}
+
+        {!props.isBlog && props.demoLink && (
+          <Button
+            variant="primary"
+            href={props.demoLink}
+            target="_blank"
+            style={{ marginLeft: "10px" }}
+          >
+            <CgWebsite /> &nbsp;
+            {"Demo"}
+          </Button>
+        )}
+        */}
+
+
+      </Card.Body>
+    </Card>
+  );
+}
+export default ProjectCards;
--- src/components/About/Professional.js
+++ src/components/About/Professional.js
@@ -1,32 +1,34 @@
 import React from "react";
 import Card from "react-bootstrap/Card";
+import { ImPointRight } from "react-icons/im";
+import { Container, Row, Col } from "react-bootstrap";
+import AboutCard from "./AboutCards";
 
-function App() {
-  return (
-    <div className="container">
-      <div className="row">
-        <div className="col-md-8">
-          <p>
-            This is a sample text paragraph. It contains some content that will appear to the left of the card. This is a sample text paragraph.This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. 
-          </p>
-          <div className="card-wrapper">
-            <Card>
-              <Card.Body>
-                <Card.Title>Card Title</Card.Title>
-                <Card.Text>
-                  This is the content of the card. You can place any relevant
-                  information, images, or components here.
-                </Card.Text>
-              </Card.Body>
-            </Card>
-          </div>
-          <p>
-            This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. This text continues below the card, and you can add more content as needed. This text continues below the card. 
-          </p>
-        </div>
-      </div>
-    </div>
-  );
+function ProfessionalAbout(){
+    return (
+        <Container fluid className="about-section">
+            <Particle/>
+            <Container>
+                <Row style={{justifyContent: "center", padding: "10px" }}>
+                    <Col md={7}
+                    style={{
+                        justifyContent: "center",
+                        paddingTop: "20px",
+                        paddingBottom: "50px",
+                    }}
+                    >
+                        Content 
+                    </Col>
+
+                    <Col md={5} className="project-card">
+                        <AboutCard
+                            header="Side-ventures"
+                            title="TeraBh.ai"
+                            description="blas"
+                        />
+                    </Col>
+                </Row>
+            </Container>
+        </Container>
+    )
 }
-
-export default App;
