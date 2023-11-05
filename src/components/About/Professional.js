ad3822efdae8ee5cdddefdc28a46d0e5bf59d003
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-06 00:10:06 +0530

    content-update

--- src/components/About/Professional.js
+++ src/components/About/Professional.js
@@ -21,18 +21,22 @@ function ProfessionalAbout(){
                         paddingBottom: "50px",
                     }}
                     >
+                        <p style={{textAlign: "justify"}}>
                         My primary research interest lies in astrophysics and planetary sciences. I have explored various facets of these fields and find the following topics particularly intriguing: stellar astrophysics and composition of planetary surfaces.
-
+                        <br/>
+                        <br/>
                         I have an affinity for working with big datasets and developing physical models. I have worked on 3D simulations of Common Envelope Evolution in binary systems, and developed a model to calculate the torque exerted on stars by the gas envelope. The manuscript for this work is currently in preparation. Additionally, I've engaged in projects related to the thermal and photometric correction and analysis of hyperspectral data from Venus (VEx/VIRTIS) and Ceres (Dawn/VIR), as well as the analysis of radar data from Venus (Magellan and Arecibo Observatory).
-
+                        <br/>
+                        <br/>
                         My experience working with substantial datasets has drawn me into the realm of machine learning and artificial intelligence. I enjoy learning about applications of machine learning techniques in complex problems. My previous work encompasses various domains, including natural language processing, atmospheric sciences, and stock market prediction. Within the field of machine learning, I am particularly interested in delving deeper into model learning and interpretability. I firmly believe that for machine learning to find its place in basic science research, models must be developed in a way that makes them physically understandable to researchers.
+                        </p>
                     </Col>
 
                     <Col md={5} className="project-card">
                         <AboutCard
                             header="Side-ventures"
                             title="TeraBh.ai"
-                            description="blas"
+                            description="ChatGPT and other utility websites and applications have shown how big of an impact can Natural Language Processing have in our day-to-day lives. Have you ever imagined how much of an impact would a personalised AI assistant have in your daily life? Yet, the currently available assistants provided by Google, Amazon, Apple etc. does not leverage the true of power of NLP. Here at TeraBh.ai, we aim to bridge the gap between these two domains. We hope to bring our Minimally Viable Product (MVP) within the end of this calendar year. <br/><br/> Want to get in touch? Mail at init.beyondcontrol@gmail.com."
                         />
                     </Col>
                 </Row>
