68f213b4ab00d50d17766e54ae17cab0511b0fa1
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-06 14:51:03 +0530

    fork link

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -15,7 +15,7 @@ function Footer() {
     <Container fluid className="footer">
       <Row>
         <Col md="4" className="footer-copywright">
-          <h3>
+          <h3 style={{padding:0, margin:0}}>
             {" "}
             <a
               href="https://github.com/soumyajit4419/Portfolio"
