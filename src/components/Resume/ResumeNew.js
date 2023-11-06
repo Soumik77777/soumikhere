12ef65e65b1266bb543b6e9f91b8f591ab1ea41e
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-06 17:32:40 +0530

    cv update

--- src/components/Resume/ResumeNew.js
+++ src/components/Resume/ResumeNew.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { Container, Row } from "react-bootstrap";
 import Button from "react-bootstrap/Button";
 import Particle from "../Particle";
-import pdf from "../../Assets/../Assets/Soumik_CV_30_10_23.pdf";
+import pdf from "../../Assets/../Assets/Soumik_CV_23_11_06.pdf";
 import { AiOutlineDownload } from "react-icons/ai";
 import { Document, Page, pdfjs } from "react-pdf";
 import "react-pdf/dist/esm/Page/AnnotationLayer.css";
