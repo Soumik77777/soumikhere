3e2b6980a4419f9416da6dd11491f14d806be048
Soumik Bhattacharyya <soumik.bhattacharyya@niser.ac.in>
2023-11-06 17:38:15 +0530

    navbar link update

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -73,7 +73,7 @@ function NavBar() {
             <Nav.Item>
               <Nav.Link
                 as={Link}
-                to="/project"
+                to="/research"
                 onClick={() => updateExpanded(false)}
               >
                 <AiOutlineFundProjectionScreen
